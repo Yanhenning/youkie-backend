7c9c569c08511824695bf9ab5a1d36d9095a46cb
yanhenning <yan.henning@loadsmart.com>
2025-05-01 16:30:11 -0300

    create the db

---
+++ app/users/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from typing import Optional
+from sqlmodel import Field, SQLModel
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True)
+    email: str = Field(index=True, unique=True)
+    password: str
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
